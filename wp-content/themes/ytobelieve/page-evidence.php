d853b11168cabb6dfd0170f98a5520f7ea07aeef
imayeti <maj407@gmail.com>
2022-09-21 08:37:43 -0400

    change the way videos populate

--- wp-content/themes/ytobelieve/page-evidence.php
+++ wp-content/themes/ytobelieve/page-evidence.php
@@ -72,71 +72,7 @@ get_header();
         while($post_query->have_posts() ) {
           $post_query->the_post();
     ?>
-
-
-    <?php if (get_field('youtube_video_id')) : $videoNum++;?>
-
-      <div class="col-12 col-md-6 col-lg-4 col-xl-3 rmt-4 text-center video-tile">
-
-        <div class="tile" >
-          <div class="mobile-no-show thumbnail-container" style="" data-toggle="modal" data-target="#myModal<?=$videoNum?>"></div>
-          <iframe style="" src="https://www.youtube.com/embed/<?php the_field('youtube_video_id') ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <h6><?php the_title(); ?></h6>
-          <h5 class="blue-text font-weight-bold">Watch Video<i class="fas fa-chevron-right"></i></h5>
-
-        </div>
-
-        <div class="modal fade "  id="myModal<?=$videoNum?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <!-- <h5 class="modal-title"></h5> -->
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <iframe id="youtubeVid" style="" src="https://www.youtube.com/embed/<?php the_field('youtube_video_id') ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    <?php endif; ?>
-
-    <?php if (get_field('vimeo_video_id')) : $videoNum++;?>
-
-      <div class="col-12 col-md-6 col-lg-4 col-xl-3 rmt-4 text-center video-tile">
-
-        <div class="tile" >
-          <div class="mobile-no-show thumbnail-container" style="" data-toggle="modal" data-target="#myModal<?=$videoNum?>"></div>
-
-              <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/<?php the_field('vimeo_video_id') ?>?h=faa0d5f5f5&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-
-          <h6><?php the_title(); ?></h6>
-          <h5 class="blue-text font-weight-bold">Watch Video<i class="fas fa-chevron-right"></i></h5>
-
-        </div>
-
-        <div class="modal fade "  id="myModal<?=$videoNum?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <!-- <h5 class="modal-title"></h5> -->
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-
-                <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/<?php the_field('vimeo_video_id') ?>?h=faa0d5f5f5&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-                  
-            </div>
-          </div>
-        </div>
-      </div>
-
-    <?php endif; ?>
-
-
+    
     <?php if (get_field('article_link')) :?>
 
     <div class="col-12 col-md-6 col-lg-4 col-xl-3 rmt-4 text-center article-tile">
@@ -145,6 +81,37 @@ get_header();
         <div class="article-bg-pic" style="background-image: url('<?php the_post_thumbnail_url() ?>');" ></div>
         <h6><?php the_title(); ?></h6>
         <h5 class="blue-text font-weight-bold">Read Article<i class="fas fa-chevron-right"></i></h5>
+      </div>
+    </div>
+
+    <?php else: ?>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-3 rmt-4 text-center video-tile">
+
+      <div class="tile" >
+        <div class="mobile-no-show thumbnail-container" style="" data-toggle="modal" data-target="#myModal<?=$videoNum?>"></div>
+
+        <?php the_field('embed'); ?>
+
+        <h6><?php the_title(); ?></h6>
+        <h5 class="blue-text font-weight-bold">Watch Video<i class="fas fa-chevron-right"></i></h5>
+
+      </div>
+
+      <div class="modal fade "  id="myModal<?=$videoNum?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <!-- <h5 class="modal-title"></h5> -->
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+
+              <?php the_field('embed'); ?>
+                
+          </div>
+        </div>
       </div>
     </div>
 
