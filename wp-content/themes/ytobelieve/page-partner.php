4a357b76a5ea1b5792d807e3bd44bb9121b244bb
Micah Jones <micah.jones@tithe.ly>
2023-02-27 18:53:48 -0500

    pdf upload

--- wp-content/themes/ytobelieve/page-partner.php
+++ wp-content/themes/ytobelieve/page-partner.php
@@ -126,7 +126,24 @@ get_header();
 
 
 </div>
+    <div class="text-center rmt-5">
+        <!--        <h2>Additional Resources</h2>-->
+        <?php
 
+        //    if( have_rows('pdfs') ):
+        //        while( have_rows('pdfs') ) : the_row();
+        //            ?>
+        <button class="y2-button" type="button" data-toggle="collapse" data-target="#collapseCoreValues" aria-expanded="false" aria-controls="collapseCoreValues">
+            <a href="<?= get_field('pdf_file') ?>" target="_blank">
+                <p><?= get_field('pdf_title') ?></p>
+            </a>
+        </button>
+        <!--        --><?php
+        //        endwhile;
+        //    endif;
+        //
+        //    ?>
+    </div>
 </section>
 
 
